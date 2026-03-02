64a64fc9c4f932d9576bcaac75640c0eb4f0e052
unknown <nmdarmstadt@gmail.com>
2026-03-02 23:58:12 +0100

    icon; cleanup

--- legacy_src/config.ts
+++
@@ -1,4 +0,0 @@
-export const GITHUB_CLIENT_ID = "PASTE_CLIENT_ID_HERE";
-export const DEV_REPO_OWNER = "yourusername";
-export const DEV_REPO_NAME = "highland-cattle-dev";
-export const DEV_REPO_BRANCH = "main";
